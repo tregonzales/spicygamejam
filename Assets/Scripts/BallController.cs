6b489727e393304ee7d584d1976c889075495d11
jeremymoss <jmoss@macalester.edu>
2020-03-21 17:09:06 -0700

    Added Bouncing Ball Prefab

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float maxBallSpeed = 5.0f;
+
+    Rigidbody playerBody;
+
+    void Start()
+    {
+        playerBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bounceIt();
+    }
+
+    void bounceIt()
+    {
+        if (Mathf.Abs(playerBody.velocity[1]) > maxBallSpeed)
+        {
+            if (playerBody.velocity[1] > 0)
+            {
+                playerBody.velocity = new Vector3(playerBody.velocity[0], maxBallSpeed, playerBody.velocity[2]);
+            }
+            else
+            {
+                playerBody.velocity = new Vector3(playerBody.velocity[0], -maxBallSpeed, playerBody.velocity[2]);
+            }
+        }
+    }
+}
